452c453bcac9813ab4b4e3ff37a5c6ea1c89797c
jeremy-wang-lin <51841335+jeremy-wang-lin@users.noreply.github.com>
2021-10-30 14:30:17 +0800

    Finish in 22. Equality for All, Redux
    Extract amount field and equals method to Money superclass

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,8 +1,6 @@
 package guru.springframework;
 
-public class Dollar {
-
-    private int amount;
+public class Dollar extends Money {
 
     public Dollar(int amount) {
         this.amount = amount;
@@ -11,11 +9,4 @@ public class Dollar {
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
-
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-
-        return amount == dollar.amount;
-    }
-
 }
---
+++ src/main/java/guru/springframework/Money.java
@@ -0,0 +1,11 @@
+package guru.springframework;
+
+public class Money {
+    protected int amount;
+
+    public boolean equals(Object object) {
+        Money dollar = (Money) object;
+
+        return amount == dollar.amount;
+    }
+}
