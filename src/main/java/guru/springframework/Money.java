1a659e961941af88d1afcf8661499a57ed27b5a6
jeremy-wang-lin <51841335+jeremy-wang-lin@users.noreply.github.com>
2021-10-30 14:41:32 +0800

    Finish in 23. Testing inequality of different currency types (Dollar & Franc)

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -4,8 +4,9 @@ public class Money {
     protected int amount;
 
     public boolean equals(Object object) {
-        Money dollar = (Money) object;
+        Money money = (Money) object;
 
-        return amount == dollar.amount;
+        return (amount == money.amount)
+                && (this.getClass().equals(object.getClass()));
     }
 }
